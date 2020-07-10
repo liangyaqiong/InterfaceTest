3b421d263621df3c541f9f047563030ae7094fd6
liangyq <1440404600@qq.com>
2020-07-10 17:59:45 +0800

    提交用例代码及报告相关代码

---
+++ Common/common_api.py
@@ -0,0 +1,83 @@
+import requests
+from Utils.local_config_utils import LocalConfig
+
+
+session = requests.session()
+
+
+
+def get_access_token_common():
+    "获取TOKEN公共类"
+    params = {"grant_type": "client_credential",
+              "appid": LocalConfig.APPID,
+              "secret": LocalConfig.SERCET
+              }
+    response =  session.get(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/token',
+                params = params )
+    TOKEN = response.json()['access_token']
+    return TOKEN
+
+
+def get_access_token(grant_type,appid,secret):
+    "获取TOKEN接口封装"
+    params = {
+        "grant_type": grant_type,
+        "appid": appid,
+        "secret":secret
+    }
+    response =  session.get(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/token',
+                params = params )
+    return response
+
+def get_access_token(TOKEN,name):
+    "获取TOKEN接口封装"
+    params={"access_token":TOKEN}
+    data ={"tag": {"name": name }}
+
+    response =  session.post(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/tags/create',
+                params = params,
+                data = data)
+    return response
+
+
+def get_access_token(TOKEN,tag_json):
+    "创建tag接口封装"
+    params={"access_token":TOKEN}
+    json_data =tag_json
+
+    response =  session.post(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/tags/create',
+                params = params,
+                json=json_data )
+    return response
+
+
+def get_access_token(TOKEN,tag_json):
+    "编辑tag接口封装"
+    params={"access_token":TOKEN}
+    json_data =tag_json
+
+    response =  session.post(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/tags/update',
+                params = params,
+                json=json_data )
+    return response
+
+def get_access_token(TOKEN,tag_json):
+    "删除tag接口封装"
+    params={"access_token":TOKEN}
+    json_data =tag_json
+
+    response =  session.post(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/tags/delete',
+                params = params,
+                json=json_data )
+    return response
+
+def get_access_token(TOKEN,JSON_PARAMS):
+    "设置用户备注名接口封装"
+    params={"access_token":TOKEN}
+    json_data =JSON_PARAMS
+
+    response =  session.post(url=LocalConfig.WEIXIN_HOST+'/cgi-bin/user/info/updateremark',
+                params = params,
+                json=json_data )
+    return response
+
---
+++ TestCase/test_weixin.py
@@ -0,0 +1,12 @@
+import unittest
+from Utils.local_config_utils import LocalConfig
+
+
+class Test_Cases_WEIXIN(unittest.TestCase):
+
+    def __init__(self):
+        pass
+    def tearDown(self) -> None:
+        pass
+    def test_get_token(self):
+        pass
