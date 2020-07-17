022218e829047b8ec4790c7672e2e71ba0e32ef5
liangyq <1440404600@qq.com>
2020-07-17 18:18:52 +0800

    提交封装request请求代码

--- Utils/request_utils.py
+++ Utils/request_utils.py
@@ -34,15 +34,16 @@ class RequestUtils:
             'response_headers':response.headers,
             'response_body':response.text
         }
-
+        print(response.url)
         return get_result
 
    def post(self,params_info):
 
         response = self.session.post(url=self.host_url+params_info['请求地址'],
-                                     params = params_info['请求参数(get)'],
-                                     data = params_info['提交数据（post）']
+                                     params = ast.literal_eval(params_info['请求参数(get)']) ,
+                                     json = ast.literal_eval( params_info['提交数据(post)'])
                                      )
+        print(params_info['提交数据(post)'])
         response.encoding = response.apparent_encoding
         if params_info['取值方式'] == 'json取值':
             value = jsonpath.jsonpath(response.json(), params_info['取值代码'])[0]
@@ -58,7 +59,7 @@ class RequestUtils:
             'response_headers': response.headers,
             'response_body': response.text
         }
-
+        print(response.url)
         return post_result
 
 
@@ -69,15 +70,16 @@ class RequestUtils:
         if  variable_list:
             for variable in variable_list:
                 params_step['请求参数(get)'] = params_step['请求参数(get)']\
-                .replace(params_step['请求参数(get)'],'"%s"' % self.temp_variables[variable[2:-1]])
+                .replace(variable,'"%s"' % self.temp_variables[variable[2:-1]])
+                print(params_step['请求参数(get)'])
         if request_type == 'get':
             result = self.get(params_step)
         elif request_type == 'post':
-            variable_list_post = re.findall('\\${\w+}', params_step['提交数据（post）'])
-            if variable_list_post:
+            variable_list_post = re.findall('\\${\w+}', params_step['提交数据(post)'])
+            if variable_list:
                for variable_post in variable_list_post:
-                   params_step['提交数据（post）'] = params_step['提交数据（post）']\
-                   .replace( params_step['提交数据（post）'],self.temp_variables[variable_post[2:-1]])
+                   params_step['提交数据(post)'] = params_step['提交数据(post)']\
+                   .replace( variable_post,'"%s"' % self.temp_variables[variable[2:-1]])
             result = self.post(params_step)
 
         else:
@@ -98,11 +100,9 @@ class RequestUtils:
 if __name__ =='__main__':
 
     case = RequestUtils()
-    case_info = [
-        {'请求方式': 'get', '请求地址': '/cgi-bin/token',
-         '请求参数(get)': '{"grant_type":"client_credential","appid":"wx55614004f367f8ca","secret":"65515b46dd758dfdb09420bb7db2c67f"}',
-         '提交数据（post）': '', '取值方式': 'json取值', '传值变量': 'token', '取值代码': '$.access_token'},
-        {'请求方式': 'post', '请求地址': '/cgi-bin/tags/delete', '请求参数(get)': '{"access_token":${token}}',
-         '提交数据（post）': '{"tag":{"id":211}}', '取值方式': '无', '传值变量': '', '取值代码': ''}
-    ]
+
+    case_info =     [ {'请求方式': 'get', '请求地址': '/cgi-bin/token','请求参数(get)': '{"grant_type":"client_credential","appid":"wx55614004f367f8ca","secret":"65515b46dd758dfdb09420bb7db2c67f"}','提交数据（post）': '', '取值方式': 'json取值', '传值变量': 'token', '取值代码': '$.access_token'},
+                      {'请求方式': 'post', '请求地址': '/cgi-bin/tags/update', '请求参数(get)': '{"access_token":${token}}','提交数据(post)': '{"tag":{"id":111,"name":"Ykfn8C47qWd12FKt1"}}', '取值方式': '无', '传值变量': '', '取值代码': ''},
+                      {'请求方式': 'post', '请求地址': '/cgi-bin/tags/create', '请求参数(get)': '{"access_token":${token}}','提交数据(post)': '{"tag" : {"name" : "uwuwbdhdbbwbdhwbdh"} }', '取值方式': '无', '传值变量': '', '取值代码': ''}]
+
     case.steps_case(case_info)
